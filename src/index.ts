6698141fbd45ca7ddffdf804ffac9af84914279b
qingzhou <qzhou@plainconcepts.com>
2017-12-13 11:12:12 -0800

    New: Add chisel rule

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @fileoverview Use Chisel to access accessibility of web sites and web applications
+ */
+
+module.exports = { chisel: require('./rules/chisel/chisel') };
---
+++ src/rules/chisel/chisel.ts
@@ -0,0 +1,172 @@
+/**
+ * @fileoverview Use Chisel to access accessibility of web sites and web applications
+ */
+
+import { Category } from 'sonarwhal/dist/src/lib/enums/category';
+import { RuleContext } from 'sonarwhal/dist/src/lib/rule-context';
+import { IRule, IRuleBuilder, ITraverseEnd, IAsyncHTMLElement, Severity } from 'sonarwhal/dist/src/lib/types';
+import { debug as d } from 'sonarwhal/dist/src/lib/utils/debug';
+import { IChiselResults, AxeNodeResult, AxeRule, IChiselDecorations, IChiselOptions } from '@mskeros/chisel';
+import { readFileAsync } from 'sonarwhal/dist/src/lib/utils/misc';
+
+const debug: debug.IDebugger = d(__filename);
+
+/*
+ * ------------------------------------------------------------------------------
+ * Public
+ * ------------------------------------------------------------------------------
+ */
+
+const rule: IRuleBuilder = {
+    create(context: RuleContext): IRule {
+
+        let chiselConfig: IChiselOptions = {};
+
+        const overrideLookUp: object = {
+            dom: ['include', 'exclude'],
+            selector: ['include', 'exclude'],
+            testsToRun: ['enableBestPracticeRules']
+        };
+
+        interface IChiselViolation extends AxeRule, IChiselDecorations { }
+
+        const loadRuleConfig = () => {
+            if (!context.ruleOptions) {
+                return;
+            }
+
+            chiselConfig = context.ruleOptions;
+        };
+
+        const generateScript = (): string => {
+            /*
+             * This is run in the page, not sonarwhal itself.
+             */
+            const script: string =
+                `function runChiselScan() {
+                    return new Promise(function (resolve, reject) {
+                        Chisel.scan(${JSON.stringify(chiselConfig, null, 2)}, function (results) {
+                            resolve(results);
+                        }, function (error) {
+                            reject(error);
+                        });
+                    });
+                }`;
+
+            return script;
+        };
+
+        const getElement = async (node: AxeNodeResult): Promise<IAsyncHTMLElement> => {
+            const selector: string = node.target[0];
+            const elements: Array<IAsyncHTMLElement> = await context.querySelectorAll(selector);
+
+            return elements[0];
+        };
+
+        const validateConfig = (config: IChiselOptions) => {
+            const warnings: Array<string> = [];
+
+            Object.keys(overrideLookUp).forEach((dominant: string) => {
+                if (config.hasOwnProperty(dominant)) {
+                    const lessers = overrideLookUp[dominant].filter((lesserProperty) => {
+                        return config.hasOwnProperty(lesserProperty);
+                    });
+
+                    if (lessers.length) {
+                        warnings.push(`${lessers.join(', ')} is ignored when ${dominant} is set.`);
+                    }
+                }
+            });
+
+            return warnings;
+        };
+
+        const validate = async (traveseEnd: ITraverseEnd) => {
+            const { resource } = traveseEnd;
+            const chiselScript: string = await readFileAsync(require.resolve('@mskeros/chisel'));
+            const runChiselScript: string = `(function() {
+                ${chiselScript};
+
+                return ${generateScript()}();
+            }())`;
+            let result: IChiselResults = null;
+
+            const warnings = validateConfig(chiselConfig);
+
+            if (warnings.length) {
+                const reportPromises = warnings.map((warning) => {
+                    return context.report(resource, null, warning, null, null, Severity.warning);
+                });
+
+                await Promise.all(reportPromises);
+            }
+
+            try {
+                result = await context.evaluate(runChiselScript);
+            } catch (error) {
+                await context.report(resource, null, `Error executing script${error.message ? `: "${error.message}"` : ''}. Please try with another connector`, null, null, Severity.warning);
+                debug('Error executing script %O', error);
+
+                return;
+            }
+
+            if (!result || !Array.isArray(result.violations)) {
+                debug(`Unable to parse axe results ${result}`);
+
+                return;
+            }
+
+            if (result.violations.length === 0) {
+                debug('No accessibility issues found');
+
+                return;
+            }
+
+            const reportPromises = result.violations.reduce((promises: Array<Promise<void>>, violation: IChiselViolation) => {
+                const elementPromises = violation.nodes.map(async (node: AxeNodeResult) => {
+                    const element: IAsyncHTMLElement = await getElement(node);
+
+                    return context.report(resource, element, violation.help);
+                });
+
+                return promises.concat(elementPromises);
+            }, []);
+
+            await Promise.all(reportPromises);
+        };
+
+        loadRuleConfig();
+
+        return { 'traverse::end': validate };
+    },
+    meta: {
+        docs: {
+            category: Category.accessibility,
+            description: `Use Chisel to access accessibility of web sites and web applications`
+        },
+        recommended: false,
+        schema: [{
+            additionalProperties: false,
+            properties: {
+                dom: { type: ['string', 'array'] },
+                enableBestPracticeRules: { type: 'boolean' },
+                exclude: {
+                    items: { type: 'string' },
+                    type: 'array'
+                },
+                include: {
+                    items: { type: 'string' },
+                    type: 'array'
+                },
+                selector: { type: 'string' },
+                testsToRun: {
+                    items: { type: 'string' },
+                    type: 'array'
+                }
+            }
+        }],
+        worksWithLocalFiles: true
+    }
+};
+
+module.exports = rule;
